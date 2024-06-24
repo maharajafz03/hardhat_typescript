1298c45c970760a606c7a12950216526203be48d
maharajafz03 <maharajafz1999@gmail.com>
2024-06-24 13:05:07 +0530

    tokens

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,3 +1,31 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.0;
+
+contract token{
+
+    string name = "token";
+    string symbol = "mtk";
+    uint totalSupply = 1000000;
+    address owner;
+
+      mapping(address => uint) public balance;
+      event Transfer(address indexed _from, address indexed _to, _value);
+
+    constructor(){
+        owner = msg.sender;
+        balance[owner] = totalSupply;
+
+    }
+
+    function transfer(address _to, uint _value) public{
+        require(balance[owner] >= _value, "insufficent fund");
+        require(_to != address(0), "invalid account");
+
+        balance[owner] -= _value;
+        balance[_to] += _value;
+
+        emit Transfer(owner, _to, _value);
+
+    }
+}
